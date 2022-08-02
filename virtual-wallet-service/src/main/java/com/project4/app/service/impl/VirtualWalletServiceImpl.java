13493b63b7e41aaa3ea2d36889c17bc8a9abeaf0
YamilRios <56850473+YamilRios@users.noreply.github.com>
2022-08-02 16:55:26 -0500

    Update VirtualWalletServiceImpl.java

--- virtual-wallet-service/src/main/java/com/project4/app/service/impl/VirtualWalletServiceImpl.java
+++ virtual-wallet-service/src/main/java/com/project4/app/service/impl/VirtualWalletServiceImpl.java
@@ -67,7 +67,7 @@ public class VirtualWalletServiceImpl implements VirtualWalletService {
 	@Override
 	@CacheEvict(cacheNames = CacheConfig.WALLET_CACHE, key = "#id")
 	public Mono<Void> delete(String id) {
-		return vwRepository.deleteById(id);
+		return vwRepository.findById(id).flatMap( x -> vwRepository.delete(x).then(Mono.just(VirtualWallet.builder().id(id).build())));
 	}
 	
 }
